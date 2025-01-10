225b667f62299b4b57277580fa9c62dc8c0d2d69
Intybyte <vaan.testwork@gmail.com>
2025-01-10 17:04:46 +0100

    Add data handling

---
+++ src/main/kotlin/me/vaan/networkMonitor/data/MachineMap.kt
@@ -0,0 +1,6 @@
+package me.vaan.networkMonitor.data
+
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem
+
+class MachineMap : Map<SlimefunItem, MultipleMachineData> by HashMap() {
+}
---
+++ src/main/kotlin/me/vaan/networkMonitor/data/MultipleMachineData.kt
@@ -0,0 +1,6 @@
+package me.vaan.networkMonitor.data
+
+data class MultipleMachineData(
+    var amount: Int = 0,
+    var active: Int = 0,
+)
